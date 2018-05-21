aabc2a01684123d0dd7d85194112b3b3f3ac0441
Sarah Strange <strange1120@gmail.com>
2018-05-21 00:07:33 -0400

    random password

--- src/Email.java
+++ src/Email.java
@@ -6,6 +6,7 @@ public class Email {
 	private String password;
 	private String department;
 	private int mailboxCapacity;
+	private int defaultPasswordLength = 10;
 	private String alternateEmail;
 	
 	public Email(String firstName, String lastName) {
@@ -13,12 +14,27 @@ public class Email {
 		this.lastName = lastName;
 		System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
 	
+		this.department = setDepartment();
+		System.out.println("Department: " + this.department);
 		
+		this.password = randomPassword(defaultPasswordLength);
+		System.out.println("Your password is: " + this.password);
+		
+	}
+	
+	private String randomPassword(int length) {
+		String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&";
+		char[] password = new char[length];
+		for (int i=0; i<length; i++ ) {
+			int  rand = (int) (Math.random() * passwordSet.length());
+			password[i] = passwordSet.charAt(rand);
+		}
+		return new String(password);
 		
 	}
 	
 	private String setDepartment() {
-		System.out.println("Enter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none");
+		System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code");
 		Scanner in = new Scanner(System.in);
 		int depChoice = in.nextInt();
 		if (depChoice == 1) {
